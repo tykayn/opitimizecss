acb8bf6f00fd41e1ca908dbcb3a9ac621ad16a77
tykayn du vortex a chats <tykayn@gmail.com>
2014-04-29 13:07:46 +0200

    ajout d'optimisation attendues

--- index.php
+++ index.php
@@ -9,12 +9,13 @@ and open the template in the editor.
         <title>Demo d'optimisation CSS</title>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <link rel="stylesheet" media="all" href="style.css" />
+        <link href="http://netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css" rel="stylesheet">
 
     </head>
     <?php
     error_reporting(E_ALL);
     //TODO get the POST data if send by user
-    $filepath = "test.css";
+    $filepath = "css/test.css";
     $file = file_get_contents($filepath);
     $file = str_replace(PHP_EOL, '', $file);
     $op = array();
@@ -30,54 +31,86 @@ and open the template in the editor.
     ?>
 
     <body>
-        <h1>Optimiseur de CSS simple</h1>
-        <div class="well">Cet optimiseur n'est pas compatible avec les média queries pour l'instant
-            <br/>L'optimisation comprend:
-            La non répétition des sélecteurs, des instructions qui se retrouvent écrasées.
-        </div>
-        <!--        <div  class="row-fluid">
-                    <div class="span5 well">
-                        <h2>Feuille pas optimisée: <?php echo $longueur_old; ?></h2>
-                        <div id="unoptimised"  class="css">
-                            <pre>
-        <?php echo nl2br($file, true); ?></pre>
-                        </div>
-                    </div>
-                    <div  class="span5 well">
-                        <h2> Feuille optimisée et rangée: <?php echo $longueur_new; ?>. gain: <?php echo $gain_compression; ?></h2>
-                        <div id="optimised" class="css" >
-                            <pre><?php echo $optimised; ?></pre>
-                        </div>
-                    </div>
+        <div class="container">
+
+
+            <h1>Optimiseur de CSS simple</h1>
+            <div class="row-fluid">
+                <div class="span5"><div class="well">Cet optimiseur n'est pas compatible avec les média queries pour l'instant
+                        <br/>L'optimisation comprend:
+                        La non répétition des sélecteurs, des instructions qui se retrouvent écrasées.
+                    </div></div>
+                <div class="span5">
+                    <form action="/" method="POST">
+                        <textarea name="lecss" id="lecss" cols="30" rows="10"></textarea>
+                        <input type="submit" value="optimiser"/>
+                    </form>
                 </div>
-                <hr/>-->
-        <?php
-        require("tpl/demo_line.php");
-        $filepath = "css/1.css";
-        require("tpl/demo_line.php");
-        $filepath = "css/2.css";
-        require("tpl/demo_line.php");
-        $filepath = "css/3.css";
-        require("tpl/demo_line.php");
-        ?>
-
-
-        <div  class="">
-            Logs
-            <div id="log" class="css" >
-                <pre><?php var_dump($optimised); ?></pre>
-                <pre><?php var_dump($file); ?></pre>
-
             </div>
+            <h2>Expected:</h2>
+            <div class="span12 well">
+                a{
+                    margin: 1px 2px 3px 4px; 
+                }
+                b{
+                    margin: 10px 0; 
+                }
+            </div>
+                <?php
+            require("tpl/demo_line.php");
+            $filepath = "css/1.css";
+            require("tpl/demo_line.php");
+            
+            ?>
+            <hr />
+            
+            <h2>Expected:</h2>
+            <div class="span12 well">
+                a{
+                    border: 1px 2px 3px 4px; 
+                }
+                b{
+                    border: 10px 0; 
+                }
+            </div>
+            <?php
+            $filepath = "css/2.css";
+            require("tpl/demo_line.php");
+            ?>
+            <hr />
+            <h2>Expected:</h2>
+            <div class="span12 well">
+                a{
+                    padding: 1px 2px 3px 4px; 
+                }
+                b{
+                    padding: 10px 0; 
+                }
+            </div>
+            <?php
+            $filepath = "css/3.css";
+            require("tpl/demo_line.php");
+            ?>
+            <hr />
 
+
+            <div  class="">
+                <h1>Logs</h1>
+                <div id="log" class="css" >
+                    <pre><?php var_dump($optimised); ?></pre>
+                    <pre><?php var_dump($file); ?></pre>
+
+                </div>
+            </div>
+            <footer>
+                <div class="well">
+                    <a href="https://github.com/tykayn">github tykayn</a>
+                    <br />
+                    <a href="http://artlemoine.com">portfolio artlemoine.com</a> 
+                </div>
+            </footer>
         </div>
-        <footer>
-            <div class="well">
-                <a href="https://github.com/tykayn">github tykayn</a>
-                <br />
-                <a href="http://artlemoine.com">portfolio artlemoine.com</a> 
-            </div>
-        </footer>
+        <script src="http://netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
     </body>
 </html>
 
--- tpl/demo_line.php
+++ tpl/demo_line.php
@@ -8,21 +8,29 @@
     $longueur_new = strlen($optimised);
     $gain_compression = (1 - round($longueur_new / $longueur_old, 2) ) * 100 . '%';
     echo '
-        <hr/><div  class="row-fluid">
-            <div class="span5 well">
-                <h2>Feuille pas optimisée: '.  $longueur_old .'</h2>
+        
+        <div  class="row-fluid">
+            <div class="span5">
+                <h2>Feuille pas optimisée:</h2>
                 <div id="unoptimised"  class="css">
                     <pre>
                         '.  nl2br($file, true) .'</pre>
                 </div>
+                <div class="stats" >
+                 '.  $longueur_old .' caractères.</div>
             </div>
-            <div  class="span5 well">
-                <h2> Feuille optimisée et rangée: '.  $longueur_new .'. gain: '.  $gain_compression .'. réécritures: '. $GLOBALS['ecrasement']  .'</h2>
+            <div  class="span5 ">
+                <h2> Feuille optimisée et rangée:</h2>
+                
                 <div id="optimised" class="css" >
                     <pre> '.$optimised.'></pre>
                 </div>
+                <div class="stats" >
+                '.  $longueur_new .' caractères. gain: '.  $gain_compression .'. réécritures: '. $GLOBALS['ecrasement']  .'
+                    </div>
             </div>
-        </div>';
+        </div>
+        <hr/>';
 
 ?>
 
